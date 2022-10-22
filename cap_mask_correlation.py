89ccadfe7226e9a940b51eb8791ac11da73dfa40
joulaqi <jamshidi3d@gmail.com>
2022-10-22 08:46:16 +0330

    graphics

--- cap_mask_correlation.py
+++ cap_mask_correlation.py
@@ -9,22 +9,23 @@ from cmb_cpu.cap import *
 from cmb_cpu.coords import *
 from cmb_cpu.measure import *
 
-D_COR2_FLAG    = 'DELTA_CORRELATION_2'
-D_VAR2_FLAG    = 'DELTA_VARIANCE_2'
-VAR_FLAG       = 'VARIANCE'
+D_CORR2_FLAG   = 'DELTA_CORRELATION_2'
+D_STD2_FLAG    = 'DELTA_STD_2'
+CORR_FLAG      = 'CORRELATION'
+STD_FLAG       = 'STANDAD_DEVIATION'
 CAP_FLAG       = 'CAP'
 STRIPE_FLAG    = 'STRIPE'
 
 t0 = time.time()
 
-nside           = 1024
-is_masked       = False
-measure_mode    = VAR_FLAG #D_VAR2_FLAG # CORRELATION_FLAG
-geom            = CAP_FLAG #STRIPE_FLAG #
+nside           = 64
+is_masked       = True
+measure_mode    = CORR_FLAG #STD_FLAG #D_CORR2_FLAG #D_STD2_FLAG
+geom            = STRIPE_FLAG #CAP_FLAG
 n_samples       = 64*3
-top_cap_size    = 5 # top cap thickness
+stripe_thickness = top_cap_size = 20
 dtheta          = 1
-sampling_range  = np.arange(10,171, dtheta)
+sampling_range  = np.arange(15, 165, dtheta)
 cacr = correlation_angle_cutoff_ratio = 2/3
 
 print("- nSide: {} | Map: {} | Measure: {} | Geometry: {}".format(nside, "MASKED" if is_masked else "INPAINTED", measure_mode, geom))
@@ -70,6 +71,13 @@ sky_data = (sky_temp, sky_pos)
 X2 = np.zeros(len(sampling_range))
 _mask = None if not is_masked else sky_mask
 
+fullsky_int = 1
+if measure_mode == CORR_FLAG or measure_mode == D_CORR2_FLAG:
+    fullsky_corr = parallel_correlation_tt(sky_data, n_samples, 4)
+    fullsky_int = np.sum(fullsky_corr ** 2)
+else:
+    fullsky_int = std_t(sky_data)
+
 if geom == CAP_FLAG:
     cap_angles = sampling_range
     # measure
@@ -77,19 +85,22 @@ if geom == CAP_FLAG:
         ca = cap_angles[i]
         print("++ Cap of size {} degrees".format(ca))
         top, bottom = get_top_bottom_caps(sky_data, ca, _mask)
-        if measure_mode == D_COR2_FLAG:
-            tctt = parallel_correlation_tt(top, n_samples, 16)
-            bctt = parallel_correlation_tt(bottom, n_samples, 16)
+        if measure_mode == D_CORR2_FLAG:
+            tctt = parallel_correlation_tt(top, n_samples, 4)
+            bctt = parallel_correlation_tt(bottom, n_samples, 4)
             max_index = int(cacr * 2 * min(ca, 180-ca) / 180 * n_samples)
             X2[i] = np.sum((tctt[:max_index] - bctt[:max_index])**2)
-        elif measure_mode == D_VAR2_FLAG:
+        elif measure_mode == D_STD2_FLAG:
             X2[i] = (std_t(top) - std_t(bottom))**2
-        elif measure_mode == VAR_FLAG:
+        elif measure_mode == STD_FLAG:
             X2[i] = std_t(top)
+        elif measure_mode == CORR_FLAG:
+            tctt = parallel_correlation_tt(top, n_samples, 4)
+            X2[i] = np.sum(tctt ** 2) / fullsky_int - 1
 
 elif geom == STRIPE_FLAG:
     # creating stripes
-    height = 1 - np.cos(top_cap_size * np.pi / 180)
+    height = 1 - np.cos(stripe_thickness * np.pi / 180)
     stripe_centers = sampling_range
     stripe_starts  = 180 / np.pi * np.arccos(np.cos(stripe_centers * np.pi / 180) + height/2)
     stripe_ends    = 180 / np.pi * np.arccos(np.cos(stripe_centers * np.pi / 180) - height/2)
@@ -102,28 +113,34 @@ elif geom == STRIPE_FLAG:
         start = stripe_starts[i]
         end = stripe_ends[i]
         stripe, rest_of_sky = get_stripe(sky_data, start, end, _mask)
-        if measure_mode == D_COR2_FLAG:
-            sctt = parallel_correlation_tt(stripe, 16)
-            rctt = parallel_correlation_tt(rest_of_sky, 16)
-            max_index = int(cacr * 2 * top_cap_size / 180 * n_samples)
+        if measure_mode == D_CORR2_FLAG:
+            sctt = parallel_correlation_tt(stripe, n_samples, 4)
+            rctt = parallel_correlation_tt(rest_of_sky, n_samples, 4)
+            max_index = int(cacr * 2 * stripe_thickness / 180 * n_samples)
             X2[i] = np.sum((sctt[:max_index] - rctt[:max_index])**2)
-        elif measure_mode == D_VAR2_FLAG:
+        elif measure_mode == D_STD2_FLAG:
             X2[i] = (std_t(stripe) - std_t(rest_of_sky))**2
-        elif measure_mode == VAR_FLAG:
+        elif measure_mode == STD_FLAG:
             X2[i] = std_t(stripe)
+        elif measure_mode == CORR_FLAG:
+            tctt = parallel_correlation_tt(stripe, n_samples, 4)
+            X2[i] = np.sum(tctt ** 2) / fullsky_int - 1
 
 # plot
 print("- Plotting")
 fig, ax = plt.subplots(1,1)
 fig.set_size_inches(8,5)
 
-if measure_mode == D_COR2_FLAG:
+if measure_mode == D_CORR2_FLAG:
     captitle = r'$\int [C_{tt}^{top}(\theta) - C_{tt}^{bottom}(\theta)]^2 d\theta$'
     strtitle = r'$\int [C_{tt}^{stripe}(\theta) - C_{tt}^{rest\,of\,sky}(\theta)]^2 d\theta$'
-elif measure_mode == D_VAR2_FLAG:
+elif measure_mode == CORR_FLAG:
+    captitle = r'$\frac{\int [C_{tt}^{top}(\theta)]^2 d\theta}{\int [C_{tt}^{total}(\theta)]^2 d\theta} - 1$'
+    strtitle = r'$\frac{\int [C_{tt}^{stripe}(\theta)]^2 d\theta}{\int [C_{tt}^{total}(\theta)]^2 d\theta} - 1$'
+elif measure_mode == D_STD2_FLAG:
     captitle = r'$[\sigma_{top}(T) - \sigma_{bottom}(T)]^2$'
     strtitle = r'$[\sigma_{stripe}(T) - \sigma_{rest\,of\,sky}(T)]^2$'
-elif measure_mode == VAR_FLAG:
+elif measure_mode == STD_FLAG:
     captitle = r'$\sigma_{top}(T)$'
     strtitle = r'$\sigma_{stripe}(T)$'
 
@@ -135,7 +152,13 @@ xlabel = capxlabel if geom == CAP_FLAG else strxlabel
 ax.set_xlabel(xlabel, fontsize=11)
 # ylabel
 ylabel = captitle if geom == CAP_FLAG else strtitle
-ylabel += r'$\>\>\>\> [\mu K]^2$'
+ylabel += r'$\>\>\>\>$'
+if measure_mode == STD_FLAG:
+    ylabel +=  r'$[\mu K]$'
+elif measure_mode == CORR_FLAG:
+    pass
+else:
+    ylabel +=  r'$[\mu K]^2$'
 ax.set_ylabel(ylabel, fontsize=12)
 # title
 title = captitle if geom == CAP_FLAG else strtitle
@@ -157,8 +180,10 @@ ax.legend(["CMB", max_point_label])
 file_name = "./output/"
 file_name += "{}".format(nside)
 file_name += "_{}".format("masked" if is_masked else "inpainted")
-file_name += "_{}".format("cap" if geom == CAP_FLAG else "{}stripe".format(top_cap_size))
-file_name += "_{}".format("corr2" if measure_mode == D_COR2_FLAG else "sigma2" if measure_mode == D_VAR2_FLAG else "sigma" )
+file_name += "_{}".format("cap" if geom == CAP_FLAG else "{}stripe".format(stripe_thickness))
+file_name += "_{}".format(  "dcorr2" if measure_mode == D_CORR2_FLAG else \
+                            "dstd2" if measure_mode == D_STD2_FLAG else \
+                            "std" if measure_mode == STD_FLAG else "corr")
 file_name += "_{}dtheta".format(dtheta)
 file_name += ".pdf"
 fig.savefig(file_name)
