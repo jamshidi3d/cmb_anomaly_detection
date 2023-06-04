correct for zero rotation

cmb_anomaly_utils/coords.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from . import const
 
 def angle_to_z(angle):
     return np.cos(angle * np.pi / 180)
@@ -44,6 +44,8 @@ def rotate_pole_to_north(vec_ndarray, pole_lat, pole_lon):
     )
     north = np.array([0.0, 0.0, 1.0])
     angle = np.arccos(np.dot(pole, north))
+    if angle < const.ANG_THRESHOLD:
+        return vec_ndarray
     axis  = np.cross(pole, north)[0]
     axis_length = np.sqrt(np.dot(axis, np.transpose(axis)))
     axis /= axis_length