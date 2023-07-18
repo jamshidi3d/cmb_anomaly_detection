cc953f2f901a2460b3ff3a6065fb2a6058f40f0e
Jam3D <jamshidi3d@gmail.com>
2023-07-19 02:50:48 +0330

    fancy parameters printing

--- read_cmb_maps_params.py
+++ read_cmb_maps_params.py
@@ -17,13 +17,43 @@ def get_inputs():
     json_inputs_file.close()
     return _inputs
 
+class bcolors:
+    HEADER    = '\033[95m'
+    OKBLUE    = '\033[94m'
+    OKCYAN    = '\033[96m'
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    FAIL      = '\033[91m'
+    ENDC      = '\033[0m'
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def print_inputs(input_dict):
-    max_key_len = 20
+    line_col   = bcolors.OKCYAN
+    txt_col   = bcolors.WARNING
+    # max key length
+    mkl = 20
+    # handy functions
+    def colorize(txt, col):
+        return col + txt + bcolors.ENDC
+    def print_line(length, color = bcolors.OKGREEN):
+        print(colorize("*" * length, color))
+    # fancy header
+    print_line(2 * mkl, line_col)
+    txt_header = "Parameters"
+    half1   = txt_header[:int(len(txt_header)/2)]
+    half2   = txt_header[int(len(txt_header)/2):]
+    _half1  = colorize("*" * (mkl - len(half1)), line_col)
+    _half2  = colorize("*" * (mkl - len(half2)), line_col)
+    print(_half1 + colorize(txt_header, txt_col) + _half2)
+    # parameters
     for key, val in zip(input_dict.keys(), input_dict.values()):
         if "comment" in key.lower():
             continue
-        print("-" + " " * (max_key_len - len(key)) + key, ":", val)
-    for i in range(2) : print("*" * 40)
+        txt_before_delim = "-" + " " * (mkl - len(key)) + colorize(str(key), txt_col)
+        print(txt_before_delim + " : " + colorize(str(val), txt_col))
+    # fancy line
+    for i in range(2) : print_line(2 * mkl, line_col)
 
 def get_cmb_pixdata(**inputs):
     sky_pix = cau.map_reader.get_data_pix_from_cmb(cmb_fpath, mask_fpath, **inputs)
